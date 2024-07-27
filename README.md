e8e4ed99b12e82736e0467a6bb18804fcaec3ccd
Afrah <afrah.hassan@inmind.ai>
2024-07-27 18:59:45 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# How To install this repository
+
+It's best to have angular cli v17 installed globally, with compatible Nodejs verions between: ^18.13.0 || ^20.9.0.
+
+    npm install
+
+# To Run the Development Backend Server
+
+We can start the sample application backend with the following command:
+
+    npm run server
+
+This is a small Node REST API server.
+
+# Run the UI
+
+    ng serve  --proxy-config ./proxy.json
+
+
---
+++ src/app/core/home/home.component.ts
@@ -0,0 +1,68 @@
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {Course} from '../../courses/model/course';
+import {Observable} from 'rxjs';
+import {defaultDialogConfig} from '../../courses/shared/default-dialog-config';
+import {EditCourseDialogComponent} from '../../courses/components/edit-course-dialog/edit-course-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
+import {map} from 'rxjs/operators';
+import {CourseEntityService} from '../../courses/services/course-entity.service';
+
+
+@Component({
+    selector: 'home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css'],
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HomeComponent implements OnInit {
+
+    promoTotal$: Observable<number>;
+
+    beginnerCourses$: Observable<Course[]>;
+
+    advancedCourses$: Observable<Course[]>;
+
+    constructor(
+      private dialog: MatDialog,
+      private coursesService: CourseEntityService) {
+
+    }
+
+    ngOnInit() {
+      this.reload();
+    }
+
+  reload() {
+
+    this.beginnerCourses$ = this.coursesService.entities$
+      .pipe(
+        map(courses => courses.filter(course => course.category == 'FRONTEND'))
+      );
+
+    this.advancedCourses$ = this.coursesService.entities$
+      .pipe(
+        map(courses => courses.filter(course => course.category == 'BACKEND'))
+      );
+
+    this.promoTotal$ = this.coursesService.entities$
+        .pipe(
+            map(courses => courses.filter(course => course.promo).length)
+        );
+
+  }
+
+  onAddCourse() {
+
+    const dialogConfig = defaultDialogConfig();
+
+    dialogConfig.data = {
+      dialogTitle:"Create Course",
+      mode: 'create'
+    };
+
+    this.dialog.open(EditCourseDialogComponent, dialogConfig);
+
+  }
+
+
+}
